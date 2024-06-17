c8232c9733e18c8a87d208033cd01c9f30540038
Otávio Bitencourt <60984811+ottaviosb@users.noreply.github.com>
2024-06-17 10:43:47 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+document.onkeydown = function(e){
+    e = e || window.event;
+    var key = e.which || e.keyCode;
+	var videoContainerElement = document.getElementById('eitv-player-container');
+	var player = videoContainerElement.player;
+    if(key===77) /*M*/{
+                if (player.isMuted()) {
+                    player.unmute();
+                }
+                else {
+                    player.mute();
+                }
+    }
+	if(key===39) /*Seta para direita*/ {
+                player.seek(Math.min(player.getDuration(), player.getCurrentTime() + 5));
+    }
+	if(key===37) /*Seta para esquerda*/{
+                player.seek(Math.min(player.getDuration(), player.getCurrentTime() - 5));
+    }
+	if(key===38) /*Seta para cima*/{
+                player.setVolume(Math.min(100, player.getVolume() + 10));
+    }
+	if(key===40) /*Seta para baixo*/{
+                player.setVolume(Math.min(100, player.getVolume() - 10));
+    }
+	if(key===80) /*P*/{
+                if (player.isPlaying()) {
+                    player.pause();
+                }
+                else {
+                    player.play();
+                }
+    }
+}
